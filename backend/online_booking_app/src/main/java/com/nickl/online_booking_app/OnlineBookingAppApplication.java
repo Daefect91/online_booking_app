3ec839332206c32c4fe7eaaf71b0517cb979c66c
Daefect91 <nic.kleynhans@gmail.com>
2025-10-20 07:57:45 +0200

    Testing backend workflow trigger

--- backend/online_booking_app/src/main/java/com/nickl/online_booking_app/OnlineBookingAppApplication.java
+++ backend/online_booking_app/src/main/java/com/nickl/online_booking_app/OnlineBookingAppApplication.java
@@ -2,7 +2,7 @@ package com.nickl.online_booking_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Testing workflow trigger
 @SpringBootApplication
 public class OnlineBookingAppApplication {
 
