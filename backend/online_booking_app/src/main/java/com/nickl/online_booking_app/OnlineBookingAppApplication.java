0eea69eb22268ff446ecd35e0af1d10ea4a7f029
Daefect91 <nic.kleynhans@gmail.com>
2025-10-22 06:03:47 +0200

    test

--- backend/online_booking_app/src/main/java/com/nickl/online_booking_app/OnlineBookingAppApplication.java
+++ backend/online_booking_app/src/main/java/com/nickl/online_booking_app/OnlineBookingAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineBookingAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hi!");
 		SpringApplication.run(OnlineBookingAppApplication.class, args);
 	}
 
