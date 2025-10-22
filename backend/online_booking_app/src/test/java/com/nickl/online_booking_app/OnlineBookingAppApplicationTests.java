ae8283a6ccfe76fee2bdfd70a6e20f0c1ab71c47
Daefect91 <nic.kleynhans@gmail.com>
2025-10-22 05:49:07 +0200

    Test backend CI trigger

--- backend/online_booking_app/src/test/java/com/nickl/online_booking_app/OnlineBookingAppApplicationTests.java
+++ backend/online_booking_app/src/test/java/com/nickl/online_booking_app/OnlineBookingAppApplicationTests.java
@@ -2,7 +2,7 @@ package com.nickl.online_booking_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//Test CI trigger
 @SpringBootTest
 class OnlineBookingAppApplicationTests {
 
